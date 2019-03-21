704cb104e59deba39873e9790d34a23c6860ab56
masterqaz <mathieu.dubreuil@outlook.com>
2019-03-21 13:42:04 -0400

    initial commit

---
+++ my-lib/projects/mylib/src/lib/mylib.module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from '@angular/core';
+import { MylibComponent } from './mylib.component';
+
+@NgModule({
+  declarations: [MylibComponent],
+  imports: [
+  ],
+  exports: [MylibComponent]
+})
+export class MylibModule { }
